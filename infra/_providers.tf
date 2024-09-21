8e4da5806fe2f60e1ef6f6b596947999bba7d554
Rob Burger <69145808+robburger-nf@users.noreply.github.com>
2024-09-21 10:56:47 +0000

    Add missing provider

--- infra/_providers.tf
+++ infra/_providers.tf
@@ -5,3 +5,14 @@
 provider "aws" {
   region = "eu-west-1"
 }
+
+# AWS - Core - Security
+provider "aws" {
+  alias = "security"
+
+  region = "eu-west-1"
+
+  assume_role {
+    role_arn = "arn:aws:iam::${module.account_security.id}:role/RootBillingCrossAccountAssumeAdmin"
+  }
+}
