72ae2c88442411dfe75042dbc2db364b501106c2
Rob Burger <69145808+robburger-nf@users.noreply.github.com>
2024-09-21 11:01:30 +0000

    Switch over to eu-west-2 region

--- infra/_providers.tf
+++ infra/_providers.tf
@@ -3,14 +3,14 @@
 ###########
 # AWS
 provider "aws" {
-  region = "eu-west-1"
+  region = "eu-west-2"
 }
 
 # AWS - Core - Security
 provider "aws" {
   alias = "security"
 
-  region = "eu-west-1"
+  region = "eu-west-2"
 
   assume_role {
     role_arn = "arn:aws:iam::${module.account_security.id}:role/RootBillingCrossAccountAssumeAdmin"
